73f9017eba1da81c3251c5debfe14e1a038268b5
Infantevvo <arockiainfant@evvotechnology.com>
2025-07-01 12:02:33 +0530

    123

--- netlify/functions/send-email.js
+++ netlify/functions/send-email.js
@@ -10,7 +10,14 @@ exports.handler = async (event) => {
     to: email,
     from: 'infantraj3211@gmail.com',
     subject: 'Thanks for contacting us!',
-    text: `Hi ${name},\n\nWe'll get back to you soon.\n\nBest,\nYour Team`
+    text: `Hi ${name},\n\nEvent Title: Cybersecurity Awareness for SMEs – Path to Cyber Essentials
+\nDate: Friday, August 01, 2025
+\nTime: 14:00 – 17:00
+\nVenue: #01-05/06, Lobby 1, Alexandra Technopark Tower A, 119967.
+\nOrganized by: Evvo Labs & Singapore Manufacturing Federations
+\nEndorsed by: Cyber Security Agency of Singapore
+\nFee: Complimentary (Pre-registration Required)
+\n\nWe'll get back to you soon.\n\nBest,\nYour Team`
   };
 console.log("Enter",msg);
   try {
